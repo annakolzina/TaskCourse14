8937d0e6d8453745e2695fc3432f9fa11bf2f795
annakolzina <56532601+annakolzina@users.noreply.github.com>
2020-10-30 18:51:06 +0400

    14 задание

---
+++ TaskCourse14/TaskCourse14/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+/*Создайте метод, который будет выводить информацию 
+ * о количестве детей у человека. Метод принимает имя человека
+ * и количество (nullable). Метод должен выводить: неизвестно, 
+ * нет детей и сообщение о количестве детей.
+*/
+
+
+
+namespace TaskCourse14
+{
+    class Human
+    {
+        private int? count;
+        private String name;
+
+        public Human(int? count, string name)
+        {
+            this.count = count;
+            this.name = name;
+        }
+
+       /* public Human(string name) 
+        {
+            this.name = name;
+        }*/
+
+
+        public String Show_children() 
+        {
+            return (count > 0) ? (count + "  детей") : (count == 0) ? ("нет детей"):("неизвестно");
+        }
+    }
+}
---
+++ TaskCourse14/TaskCourse14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaskCourse14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human obj = new Human(0,"Ирина");
+            Console.WriteLine(obj.Show_children());
+        }
+    }
+}
